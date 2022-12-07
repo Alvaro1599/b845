383b6eb0a4cd3f861ee0a0722e34b583087cb055
Alvaro1599 <alvaro1599@gmail.com>
2022-12-07 10:40:08 -0500

    hoist

--- src/database/datasource.ts
+++ src/database/datasource.ts
@@ -26,9 +26,7 @@ const options: DataSourceOptions & SeederOptions = {
   migrations: [path.join(__dirname + "/migrations/*{.js,.ts}")],
   entities: [path.join(__dirname + "/../modules/**/entities/*{.js,.ts}")],
   logging:true,
-  extra:{
-    socketPath: process.env.DB_HOST
-  }
+
 };
 
 const AppDataSource: DataSource = new DataSource(options);
